9975f1cca116e6879ca1d8939ef00ce775c98e04
temir <temirlan020106@gmail.com>
2025-05-08 12:37:39 +0600

    fix: header buttons

---
+++ README.md
@@ -0,0 +1,12 @@
+# Geeks - React Webpage
+
+This is a simple React project with Webpack configuration. It features a loot box simulation where you can spin and get random items.
+
+## Installation
+
+To get started, clone the repository and install dependencies:
+
+```bash
+git clone https://github.com/yourusername/geeks.git
+cd geeks
+npm install
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import cs2logo from '../images/cs2logoSvg.svg';
 import '../styles/Header.css'
 import {Link} from "react-router-dom";
 
-
 export const Header = () => {
     return (
         <div className="header">
             <Link to='/' id='logo'><img className='logo' src={cs2logo} alt="CS2 Logo" /></Link>
             <Link to='/'>Main</Link>
             <Link to='/cases'>Cases</Link>
-            <button className='header-btn'>Click me!</button>
+            <Link to='/'>About us</Link>
+            <Link to='/'>Contact us</Link>
         </div>
     );
 };
